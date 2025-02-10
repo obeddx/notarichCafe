36398ff6084c793648bdfeb8bdfc81a917071773
unknown <obeddanny17@gmail.com>
2025-02-10 14:16:54 +0700

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// import NavbarGlass from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* <NavbarGlass/> */}
         {children}
       </body>
     </html>
