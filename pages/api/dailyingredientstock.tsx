7fc0ab3a2825ef3e66431531e96f4c841008d8f5
J D <immanueldarmawan@gmail.com>
2025-02-18 14:34:20 +0700

    perbaikan sedikit di api cafe

--- pages/api/dailyingredientstock.tsx
+++ pages/api/dailyingredientstock.tsx
@@ -6,28 +6,33 @@ const prisma = new PrismaClient();
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { startDate, endDate } = req.query;
 
-  // Memeriksa apakah startDate dan endDate ada dan dalam format yang benar
-  if (!startDate || !endDate || typeof startDate !== "string" || typeof endDate !== "string") {
-    return res.status(400).json({ error: "Query parameter 'startDate' dan 'endDate' diperlukan." });
+  if (!startDate || typeof startDate !== "string") {
+    return res.status(400).json({ error: "Query parameter 'startDate' diperlukan." });
   }
 
-  // Parsing startDate dan endDate menjadi objek Date
+  // Parsing startDate
   const parsedStartDate = new Date(startDate);
-  parsedStartDate.setHours(0, 0, 0, 0); // Menentukan waktu mulai dari jam 00:00:00
+  parsedStartDate.setHours(0, 0, 0, 0);
 
-  const parsedEndDate = new Date(endDate);
-  parsedEndDate.setHours(23, 59, 59, 999); // Menentukan waktu akhir pada jam 23:59:59
+  // Jika endDate tidak diberikan, gunakan startDate sebagai endDate
+  let parsedEndDate: Date;
+  if (!endDate || typeof endDate !== "string") {
+    parsedEndDate = new Date(startDate);
+    parsedEndDate.setHours(23, 59, 59, 999);
+  } else {
+    parsedEndDate = new Date(endDate);
+    parsedEndDate.setHours(23, 59, 59, 999);
+  }
 
   try {
-    // Mengambil data berdasarkan rentang waktu yang diberikan oleh startDate dan endDate
     const data = await prisma.dailyIngredientStock.findMany({
       where: {
         date: {
-          gte: parsedStartDate, // Greater than or equal to startDate
-          lte: parsedEndDate, // Less than or equal to endDate
+          gte: parsedStartDate,
+          lte: parsedEndDate,
         },
       },
-      include: { ingredient: true }, // Menyertakan data ingredient agar bisa ditampilkan
+      include: { ingredient: true },
     });
 
     res.status(200).json(data);
